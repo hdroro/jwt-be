0ea5daa2cf91751f651f422d1e9b6fb09da8a6ff
hongdiem_roro <mingukmanse673@gmail.com>
2024-01-16 09:25:02 +0700

    relationships

--- src/service/userService.js
+++ src/service/userService.js
@@ -31,59 +31,98 @@ const createNewUser = async (email, password, username) => {
   }
 };
 
-const getUserList = () => {
-  return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM user", (error, results, fields) => {
-      if (error) {
-        console.error("Error fetching user list:", error);
-        reject(error);
-      } else {
-        resolve(results);
-      }
+const getUserList = async () => {
+  let newUser = await db.User.findOne({
+    where: { id: 1 },
+    attributes: ["id", "username", "email"],
+    include: { model: db.Group, attributes: ["id", "name", "description"] },
+    raw: true,
+    nest: true,
+  });
+
+  let roles = await db.Role.findAll({
+    include: { model: db.Group, where: { id: 1 } },
+    raw: true,
+    nest: true,
+  });
+
+  console.log(">>Test newUser", newUser);
+  console.log(">>Test roles", roles);
+
+  let users = [];
+  users = await db.User.findAll();
+  return users;
+  // return new Promise((resolve, reject) => {
+  //   connection.query("SELECT * FROM user", (error, results, fields) => {
+  //     if (error) {
+  //       console.error("Error fetching user list:", error);
+  //       reject(error);
+  //     } else {
+  //       resolve(results) ;
+  //     }
+  //   });
+  // });
+};
+
+const getUserById = async (id) => {
+  try {
+    const user = await db.User.findOne({
+      where: {
+        id: id,
+      },
     });
-  });
+    return user;
+  } catch (error) {
+    console.error("Error fetching user by ID:", error);
+    throw error;
+  }
 };
 
-const getUserById = (id) => {
-  return new Promise((resolve, reject) => {
-    connection.query(
-      "SELECT * FROM user where id = ?",
-      [id],
-      (error, results, fields) => {
-        if (error) {
-          console.error("Error fetching user list:", error);
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      }
-    );
-  });
+const deleteUser = async (id) => {
+  // connection.query(
+  //   "DELETE FROM user where id = ?",
+  //   [id],
+  //   (error, results, fields) => {
+  //     if (error) {
+  //       console.error("Error creating new user:", error);
+  //     }
+  //     console.log("User deleted successfully");
+  //   }
+  // );
+  try {
+    await db.User.destroy({
+      where: {
+        id: id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const deleteUser = (id) => {
-  connection.query(
-    "DELETE FROM user where id = ?",
-    [id],
-    (error, results, fields) => {
-      if (error) {
-        console.error("Error creating new user:", error);
-      }
-      console.log("User deleted successfully");
+const editNewUser = async (email, username, id) => {
+  try {
+    const user = await db.User.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (user) {
+      user.email = email;
+      user.username = username;
+
+      await user.save();
+
+      return user;
+    } else {
+      console.error("User not found");
+      return null;
     }
-  );
-};
-
-const editNewUser = (email, username, id) => {
-  connection.query(
-    "UPDATE user SET email = ?, username = ? where id = ?",
-    [email, username, id],
-    (error, results, fields) => {
-      if (error) {
-        console.error("Error creating new user:", error);
-      }
-    }
-  );
+  } catch (error) {
+    console.error("Error editing user:", error);
+    throw error;
+  }
 };
 
 module.exports = {
